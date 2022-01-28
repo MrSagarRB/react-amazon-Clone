cbdc83ed37c2188bd38b3ef45249bf4b05b79971
Sagar Borude <sagar.html5@gmail.com>
2022-01-28 22:07:06 +0530

    Header Section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
       <Header />
       <div className='banner'>
-        i<img src="" />
+        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/gateway-2015/amazonshop/Desktop_Banner_Recruitment_Website.jpg" />
 
       </div>
      
